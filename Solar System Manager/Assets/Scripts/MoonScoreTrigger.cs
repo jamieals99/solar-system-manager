c119f668f5a372c63a2c715424b0127c7ed2c40a
Snork182 <56980246+Snork182@users.noreply.github.com>
2019-10-25 21:07:36 +0100

    Moon scoring, edited scripts, moved pictures
    Created a moon scoring script.
    Edited some other scripts.
    Moved pictures into their own folder.

---
+++ Solar System Manager/Assets/Scripts/MoonScoreTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MoonScoreTrigger : MonoBehaviour
+{
+    private int score = -10; // Score starts at -10 as object starts in triggerbox.
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Moon") // If moon enters trigger then it'll add points, planet won't add points.
+        {
+            score = score + 10; // Increases score by 100 whenever an object enters the triggerbox.
+            Debug.Log("Moon score: " + score); // Displays score to the debug log.
+        }
+    }
+}
--- Solar System Manager/Assets/Scripts/ScoreTrigger.cs
+++ Solar System Manager/Assets/Scripts/ScoreTrigger.cs
@@ -4,11 +4,13 @@ public class ScoreTrigger : MonoBehaviour
 {
     private int score = -100; // Score starts at -100 as object starts in triggerbox.
 
-
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider collision)
     {
-        score = score + 100; // Increases score by 100 whenever an object enters the triggerbox.
-        Debug.Log(score); // Displays score to the debug log.
+        if (collision.gameObject.tag=="Planet") // If planet enters trigger then it'll add points, moon won't add points.
+        {
+            score = score + 100; // Increases score by 100 whenever an object enters the triggerbox.
+            Debug.Log("Planet score: " + score); // Displays score to the debug log.
+        }
     }
 
 }
