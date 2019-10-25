e2d7d88f02d56b1186aee01882190fe69a6f726e
Snork182 <56980246+Snork182@users.noreply.github.com>
2019-10-25 19:01:48 +0100

    Added moon and skybox
    Commented on score trigger.
    Added skybox.
    Added moon.

--- Solar System Manager/Assets/Scripts/ScoreTrigger.cs
+++ Solar System Manager/Assets/Scripts/ScoreTrigger.cs
@@ -2,13 +2,13 @@
 
 public class ScoreTrigger : MonoBehaviour
 {
-    private int score = -100;
+    private int score = -100; // Score starts at -100 as object starts in triggerbox.
 
 
     void OnTriggerEnter()
     {
-        score = score + 100;
-        Debug.Log(score);
+        score = score + 100; // Increases score by 100 whenever an object enters the triggerbox.
+        Debug.Log(score); // Displays score to the debug log.
     }
 
 }
