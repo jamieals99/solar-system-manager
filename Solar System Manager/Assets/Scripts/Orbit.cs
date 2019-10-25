0e831ea8f8472c52e6f621fe5f82e937d39b38e5
Snork182 <56980246+Snork182@users.noreply.github.com>
2019-10-25 16:05:17 +0100

    Added objects and scripts
    Objects: a star and planet.
    Scripts for rotation of spherical bodies and orbits.
    Few shitty placeholder textures.

---
+++ Solar System Manager/Assets/Scripts/Orbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centre;
+    [SerializeField]
+    private Vector3 axis;
+    [SerializeField]
+    private float radius = 2f;
+    [SerializeField]
+    private float radiusSpeed = 0.5f;
+    [SerializeField]
+    private float rotationSpeed = 60f;
+
+    void Start()
+    {
+        transform.position = (transform.position - centre.position).normalized * radius + centre.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(centre.position, axis, rotationSpeed * Time.deltaTime);
+        var desiredPosition = (transform.position - centre.position).normalized * radius + centre.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
---
+++ Solar System Manager/Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform rotater;
+    [SerializeField]
+    private float rotationSpeed = 60f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotater.Rotate(0, (rotationSpeed * Time.deltaTime), 0);
+    }
+}
