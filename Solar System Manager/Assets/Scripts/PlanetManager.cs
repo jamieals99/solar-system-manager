a4a7c2708df6893cd73db21e78d286bb91b4037b
Gueynar <34599226+Gueynar@users.noreply.github.com>
2019-10-26 21:25:22 +0100

    PlanetAddition Function

---
+++ Solar System Manager/Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlanetManager : MonoBehaviour
+{
+    //public GameObject planet;
+    public GameObject[] planets; // Array of all planets in order
+    public GameObject[] orbits; // Array of all orbits in order
+    private int planetNumber = 0; // reference to array, starts at 0, counts up when Planets get added
+    private int orbitNumber = 0; //reference to array, starts at 0, counts up when Orbits get added
+   
+    private Camera mainCamera;
+    private float startFOV = 20;
+    public float fovAddition = 15; // Amount by which FOC increases when creating a planet
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();      
+    }
+
+
+    public void CreatePlanet()
+    {
+        planets[planetNumber].SetActive(true);
+        orbits[orbitNumber].SetActive(true);
+
+        orbitNumber++;
+        planetNumber++;
+
+        mainCamera.fieldOfView = startFOV;
+
+        startFOV = startFOV + fovAddition;
+       
+    }
+}
