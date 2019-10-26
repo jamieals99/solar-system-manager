4f76d159d5b9cfb590ea10e880fd8b6a1adad001
Jamie <56042392+jamieals99@users.noreply.github.com>
2019-10-26 18:27:51 +0100

    Revert "Marc Upload"
    This reverts commit 412a78dc72a2415fff3943c07cdecedc34803875, reversing
    changes made to c2d2a8cec8cc7f3a0db146dacb9ca240e7052680.

--- Solar System Manager/Assets/Scripts/ScoreTrigger.cs
+++ Solar System Manager/Assets/Scripts/ScoreTrigger.cs
@@ -2,14 +2,14 @@
 
 public class ScoreTrigger : MonoBehaviour
 {
-    public static float planetScore = -100; // Score starts at -100 as object starts in triggerbox.
+    private int score = -100; // Score starts at -100 as object starts in triggerbox.
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag=="Planet") // If planet enters trigger then it'll add points, moon won't add points.
         {
-            planetScore += 12.5f * collision.gameObject.transform.lossyScale.x * collision.gameObject.GetComponent<Orbit>().radius; // Increases score by 100 whenever an object enters the triggerbox.
-            Debug.Log("Planet score: " + planetScore); // Displays score to the debug log.
+            score = score + 100; // Increases score by 100 whenever an object enters the triggerbox.
+            Debug.Log("Planet score: " + score); // Displays score to the debug log.
         }
     }
 
