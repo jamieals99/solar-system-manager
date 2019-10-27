02d3c7770430b24d8a6569d23594a69c20095d26
Jamie <56042392+jamieals99@users.noreply.github.com>
2019-10-27 17:45:23 +0000

    FInal changes

--- Solar System Manager/Assets/Scripts/Camera1.cs
+++ Solar System Manager/Assets/Scripts/Camera1.cs
@@ -9,68 +9,37 @@ public class Camera1 : MonoBehaviour
     public GameObject CameraLocation;
     public Vector3 Offset;
     public Vector3 StartLocation;
-    public Vector3 AfterhitLocation;
     public Vector3 CameraPosition2 = new Vector3(0f, 90f, 16f);
     public float xAxis, yAxis, zAxis;
     public Vector3 StartRotation;
     public float StartXRotation = 15f;
     public float StartYRotation = 0f;
     public float StartZRotation = 0f;
-    public Vector3 HitLocation;
-    public bool Apressed = true;
-    public Orbit orbit;
-    public int Acounter = 0;
-    public PlanetManager manager;
     // Start is called before the first frame update
-    public void Start()
+    void Start()
     {
-        manager = GameObject.Find("PlanetManager").GetComponent<PlanetManager>();
         StartLocation = CameraLocation.transform.position;
         StartRotation = new Vector3(StartXRotation, StartYRotation, StartZRotation);
         Debug.Log("Location: " + StartLocation);
         Offset = CameraLocation.transform.position - PlanetLocation.transform.position;
+        transform.position = StartLocation + Offset;
     }
-    public Vector3 SwapCameraLocation(int counter)
-    {
-        if (Acounter == -1)
-        {
-            AfterhitLocation = StartLocation;
-            return AfterhitLocation + Offset;
-        }
-        else
-        {
-            AfterhitLocation = manager.planets[counter].transform.position;
-            Debug.Log(manager.planets[Acounter].transform.position);
-            Debug.Log(AfterhitLocation);
-            return AfterhitLocation + Offset;
-        }
-    }
+
     // Update is called once per frame
     void Update()
     {
-
-        transform.position = SwapCameraLocation(Acounter);
+        /*
         if (Input.GetKeyDown("a"))
         {
-            if (Acounter > 8)
-            {
-                Acounter = 8;
-            }
-            else
-            {
-                Acounter += 1;
-            }
+            transform.position = StartLocation;
+            transform.position += CameraPosition2;
+            transform.Rotate(75f, 0f, 0f);
         }
-        if (Input.GetKeyDown("s"))
+        if (Input.GetKeyUp("a"))
         {
-            if(Acounter < -1)
-            {
-                Acounter = -1;
-            }
-            else
-            {
-                Acounter -= 1;
-            }
-        }
+            transform.Rotate(-75f, 0f, 0f);
+            transform.position = StartLocation;
+            transform.position += Offset;
+        }*/
     }
 }
---
+++ Solar System Manager/Assets/Scripts/EndGame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] Transform gameUI, endUI;
+
+    [SerializeField] Text victoryScoreText;
+
+    float timer = Manager.timer;
+    float victoryScore;
+    bool gameEnd = false;
+
+    void Update()
+    {
+        timer = Manager.timer;
+        if (timer == 0&& !gameEnd)
+        {
+            victoryScore = Mathf.Round(ScoreTrigger.Score);
+            gameEnd = true;
+            DisableUI();
+            EndScreen();
+        }
+    }
+
+    void DisableUI()
+    {
+        gameUI.gameObject.SetActive(false);
+    }
+
+    void EndScreen()
+    {
+        endUI.gameObject.SetActive(true);
+        victoryScoreText.text = "Score: " + victoryScore;
+    }
+}
